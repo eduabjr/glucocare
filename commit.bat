ad4eb56762ffc7a6d5516cdce580e9406e3d7d98
Eduardo <eduardo.junior1@uscsonline.com.br>
2025-10-10 00:37:28 -0300

    Update: 10/10/2025  0:37:28,68

--- commit.bat
+++ commit.bat
@@ -1,3 +1,17 @@
 @echo off
-cd /d "C:\Users\Usuário\Desktop\glucocare"
-bash commit.sh
+cd /d "%~dp0"
+
+echo Configurando repositórios remotos...
+git remote remove origin 2>nul
+git remote add origin https://github.com/eduardofamilia01-hub/glucocare.git
+git remote add eduabjr https://github.com/eduabjr/glucocare.git
+
+echo Fazendo commit e push...
+git add .
+for /f "tokens=1-6 delims=: " %%a in ("%time%") do set timestamp=%%a%%b%%c
+git commit -m "Update: %date% %time%"
+git push origin main
+git push eduabjr main
+
+echo Concluído!
+pause
